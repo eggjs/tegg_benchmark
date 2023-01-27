676eab8d30fa3ba0f21d5dc2e44edd9c083c3dc4
零弌 <zhubin.gzb@antfin.com>
2023-01-27 14:24:01 +0800

    benchmark: add more egg case

---
+++ egg_10/generate_benchmark.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
+const pwd = __dirname;
+const moduleDir = path.join(pwd, 'app/service');
+const controllerDir = path.join(pwd, 'app/controller');
+
+function writeFile(index) {
+  const fileName = `egg_service_${index}.js`;
+  const file = `const { Service } = require('egg');
+
+module.exports = class EggService${index} extends Service {
+  hello() {
+    // ...
+  }
+}
+`;
+  fs.writeFileSync(path.join(moduleDir, fileName), file);
+}
+
+function writeRouter(counts) {
+  const routerString = [];
+  for (let i = 0; i < counts.length; i++) {
+    const count = counts[i];
+    routerString.push(`  app.get('/hello${count}', controller.eggController_${count}.hello);`);
+  }
+
+  const template =
+`module.exports = function (app) {
+  const { controller } = app;
+${routerString.join(os.EOL)}
+};
+`;
+  fs.writeFileSync(path.join(pwd, 'app/router.js'), template);
+}
+
+for (let i = 0; i < 10; i++) {
+  writeFile(i);
+}
+
+
+function generateController(count) {
+  const touchString = [];
+  for (let i = 0; i < count; i++) {
+    touchString.push(`    this.ctx.service.eggService_${i}.hello();`);
+  }
+
+  const template =
+    `const { Controller } = require('egg');
+
+module.exports = class EggController1 extends Controller {
+  async hello() {
+  if (this.ctx.query.touch) {
+${touchString.join(os.EOL)}
+  }
+    this.ctx.body = 'hello,egg${count}';
+  }
+}
+`;
+  fs.writeFileSync(path.join(controllerDir, `egg_controller_${count}.js`), template);
+}
+
+generateController(1);
+generateController(10);
+writeRouter([1, 10]);
--- wrk.sh
+++ wrk.sh
@@ -6,7 +6,19 @@ npx lerna run start
 
 sleep 30
 
-for port in 7001 7002 7003
+for port_count in "7001 1" "7002 10" "7003 100" "7004 1000" "7005 10000"
+do
+  port_count=($port_count)
+  port=${port_count[0]}
+  count=${port_count[1]}
+ for touch in "" "=true"
+ do
+   echo "wrk -c 25 -d 60 -t 25 http://127.0.0.1:${port}/hello${count}?touch${touch}"
+   wrk -c 25 -d 60 -t 25 http://127.0.0.1:${port}/hello${count}?touch${touch}
+ done
+done
+
+for port in 7006 7007
 do
  for touch in "" "=true"
  do
